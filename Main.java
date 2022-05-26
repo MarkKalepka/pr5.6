636f8c05cb445688018a4d84b917937c6d846f62
MarkKalepka <horizon248markk@gmail.com>
2022-05-26 09:35:42 +0300

    First commit

---
+++ Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a=45, b=15;
+        int c=a/b ;
+        int d=a%b;
+        System.out.println("c="+c);
+        System.out.println("d="+d);
+
+        int x=a+b;
+        System.out.println("x="+x);
+
+        double y=4.25;
+        System.out.println(Math.round(y) );
+
+
+    }
+}
